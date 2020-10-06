4fb52e356669b57214a5e4e133a8c5f0b7415f98
Anna Sokolova <sokolovaskorik@gmail.com>
2020-10-06 12:40:23 +0300

    Добавляет решение задачи 5

---
+++ js/array-function-task5.js
@@ -0,0 +1,16 @@
+function checkForSpam(message) {
+  'use strict';
+
+  const isInMessage =
+    message.toLowerCase().includes('spam') ||
+    message.toLowerCase().includes('sale');
+  return isInMessage ? true : false;
+}
+
+console.log(checkForSpam('Latest technology news'));
+
+console.log(checkForSpam('JavaScript weekly newsletter'));
+
+console.log(checkForSpam('Get best sale offers now!'));
+
+console.log(checkForSpam('[SPAM] How to earn fast money?'));
